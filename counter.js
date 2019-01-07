8d16ecd24f3834f0d23b9d59ae98ab91c5c478c4
Tyrone Saulon <tyrone.saulon@hotmail.com>
2019-01-06 20:42:32 -0500

    Renaming file

--- counter.js
+++ counter.js
@@ -1,10 +1,10 @@
 /**
-* @desc This is a function which simulates an incremental counter using a closure.
+* @desc This is an exported function which simulates an incremental counter using a closure.
 * @param {Number} start This is a param which recieves a Number value and defaults to 0.
 * @param {Number} increment This is a param which recieves a Number value and defaults to a 1.
 * @return {Function} This returns a function closure where the closure returns the incremented value
 */
-function counter(start = 0, increment = 1) {
+module.exports = function counter(start = 0, increment = 1) {
 
     let counter = start;
 
@@ -12,6 +12,3 @@ function counter(start = 0, increment = 1) {
         return counter += increment;
     };
 }
-
-let count = counter(0);
-console.log(count());
