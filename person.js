cbdd3fc8361ee53bcde00a0cebd2c143d50d0292
Tyrone Saulon <tyrone.saulon@hotmail.com>
2019-01-07 10:22:32 -0500

    Finalizing comment conventions for Person module

--- person.js
+++ person.js
@@ -1,7 +1,7 @@
-
 /**
- * @param {String} name An argument which represents the name of the person.
- * @param {Number} age An argument which represents the age of the person.
+ * Person class which encapsulates some characteristics of a person.
+ * @param {String} m_name An argument which represents the name of the person.
+ * @param {Number} m_age An argument which represents the age of the person.
  * @constructor
  */
 const Person = class Person {
@@ -11,20 +11,22 @@ const Person = class Person {
         this.m_age = age;
     }
 
+    /** @type {Number} */
     get age() { return this.m_age; }
     set age(age) { this.m_age = age; }
+    /** @type {String} */
     get name() { return this.m_name; }
     set name(name) { this.m_name = name; }
 
     /**
-     * @return {String} Returns a human-readable representation of the Person object.
+     * @async
+     * @desc This function resolves a human-readable representation of the Person object.
+     * @return {Promise} Returns a Promise object which describes the Person object.
      */
     async describe() {
-        
         if(this.age > 0 && this.name.length > 0) {
             return `${this.name} is ${this.age} years old.`;
         }
-
     }
 }
 
