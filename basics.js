ffa269e2d568c35c9d7ddddcf1f8e64491d97d0c
Tyrone Saulon <tyrone.saulon@hotmail.com>
2019-01-06 20:34:51 -0500

    Removing some comments

--- basics.js
+++ basics.js
@@ -3,7 +3,6 @@
 * @param {Number} start This is a param which recieves a Number value and defaults to 0.
 * @param {Number} increment This is a param which recieves a Number value and defaults to a 1.
 * @return {Function} This returns a function closure where the closure returns the incremented value
-* @code "bar"
 */
 function counter(start = 0, increment = 1) {
 
@@ -14,9 +13,5 @@ function counter(start = 0, increment = 1) {
     };
 }
 
-/** 
- * The local counter variable. 
- * @type {Number} 
- */
 let count = counter(0);
 console.log(count());
